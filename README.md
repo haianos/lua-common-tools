04fc9ca951d95b4909cfb0510009f021e1831a70
Enea Scioni <enea.scioni@unife.it>
2015-09-22 11:56:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Lua-common-tools
+=================
+
+Collection of common Lua script, for all the seasons!
+
+Authors
+========
+
+The script's author is reported in each single script.
+Some scripts have been taken (and modified) from others;
+the original author (and license) is indicated in the script itself.
+Otherwise, main contribuitors are:
+
+  * Markus Klotzbuecher
+  * Enea Scioni
+
+Contents
+========
+
+   * `utils`: a collection of short useful lua functions
+   * `ansicolors`: colorful printouts
+   * `uoo`: micro-Object-Oriented utility
+
+Install
+=======
+
+## Manual
+
+  * just copy the scripts in your `LUA_PATH` environment variable
+
+## Via `luarocks`
+
+Install all as `rock` using `luarocks`
+
+```
+luarocks install --local
+```
+
+**Note** you can remove the option `--local` for a system installation,
+but you may need `sudo` rights.
+
---
+++ uoo.lua
@@ -0,0 +1,76 @@
+--- imported from umf.lua project
+-- Authors: 
+--(C) 2015 Enea Scioni
+--(C) 2013 Markus Klotzbuecher
+
+--- microObjects, heavily inspired by middleclass.
+local function __class(name, super)   local klass = { name=name, superclass=super, static = {}, iops={}, __class_identifier=true }
+   local iops = klass.iops
+   iops.__index = iops
+
+   if super then
+      local superStatic = super.static
+      setmetatable(iops, super.iops)
+      setmetatable(klass.static, { __index = function(_,k) return iops[k] or superStatic[k] end })
+   else
+      setmetatable(klass.static, { __index = function(_,k) return iops[k] end })
+   end
+
+  setmetatable(klass, {
+    __tostring = function() return "class " .. klass.name end,
+    __index    = klass.static,
+    __newindex = klass.iops,
+    __call     = function(self, ...) return self:new(...) end
+  })
+
+  klass.class=function(_) return klass end -- enable access to class
+  return klass
+end
+
+Object = __class("Object")
+function class(name, super) return __class(name, super or Object) end
+
+function Object.static:new(t)
+   local obj = setmetatable(t or {}, self.iops)
+   obj:init()
+   return obj
+end
+
+function Object.static:super() return self.superclass end
+function Object.static:classname() return self.name end
+function Object.static:type() return 'class' end
+function Object:init() return end
+  
+--- Return the uoo type ('class' or 'instance')
+function uoo_type(x)
+   if not x then return false end
+   local mt = getmetatable(x)
+   if mt and mt.__index==x.static then return 'class'
+   elseif mt and mt.class then return 'instance' end
+   return false
+end
+
+--- Return the class name of the given object.
+function uoo_class(o)
+   if uoo_type(o) == 'instance' then
+      return o:class().name
+   end
+   return false
+end
+
+function subclass_of(subklass, klass)
+   function sc_of(sc, k)
+      if not sc then return false end
+      if sc==k then return true end
+      return sc_of(sc:super(), k)
+   end
+   assert(uoo_type(subklass)=='class', "subclass_of: first argument not a class")
+   assert(uoo_type(klass)=='class', "subclass_of: second argument not a class")
+   return sc_of(subklass, klass)
+end
+
+function instance_of(klass, obj)
+   assert(uoo_type(klass)=='class', "instance_of: first argument not a class")
+   assert(uoo_type(obj)=='instance', "instance_of: second argument not an instance")
+   return subclass_of(obj:class(), klass)
+end
